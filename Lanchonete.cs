35f6d2df60b52914990efc02f36866d2477188c3
GabryelVelli <143671696+GabryelVelli@users.noreply.github.com>
2024-11-11 14:47:08 -0300

    Add files via upload

---
+++ Lanchonete.cs
@@ -0,0 +1,99 @@
+﻿using ProvaLanchonete.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProvaLanchonete.Entities
+{
+    internal class Lanchonete
+    {
+        private List<Lanches> lanchesDisponiveis = new List<Lanches>();
+        private Dictionary<Lanches, int> pedidoAtual = new Dictionary<Lanches, int>();
+
+
+        public void AdicionarLanche(string nome, string descricao, decimal preco)
+        {
+
+            if (lanchesDisponiveis.Count <= 9)
+            {
+                Lanches lanche = new Lanches(nome, descricao, preco);
+                lanchesDisponiveis.Add(lanche); //adiciona o lanche
+                Console.WriteLine($"{lanche.Nome} foi Adicionado!");
+                Console.WriteLine("--------------------");
+            }
+            else
+            {
+                Console.WriteLine("Limite de Lanches adicionado Atingido!");
+                Console.WriteLine("--------------------");
+            }
+        }
+
+        public void Cardapio()
+        {
+            if (lanchesDisponiveis.Count == 0)
+            {
+                Console.WriteLine("Sem Lanches Disponiveis");
+                Console.WriteLine("--------------------");
+            }
+            else
+            {
+                Console.WriteLine("Lanches Disponíveis");
+                int index = 1; //index começando no 1.
+                foreach (var lanches in lanchesDisponiveis)
+                {
+                    Console.WriteLine($"{index}. {lanches}");
+                    index++;
+                    Console.WriteLine("--------------------");
+                }
+            }
+        }
+
+        public void Pedido(int indiceLanche)
+        {
+            if (indiceLanche >= 1 && indiceLanche <= lanchesDisponiveis.Count)
+            {
+                Lanches lancheEscolhido = lanchesDisponiveis[indiceLanche - 1];
+                if (pedidoAtual.ContainsKey(lancheEscolhido))
+                {
+                    pedidoAtual[lancheEscolhido]++;
+                }
+                else
+                {
+                    pedidoAtual[lancheEscolhido] = 1;
+                }
+                Console.WriteLine($"{lancheEscolhido.Nome} escolhido para seu pedido");
+                Console.WriteLine("--------------------");
+            }
+            else
+            {
+                Console.WriteLine("Opção de lanche inválido");
+                Console.WriteLine("--------------------");
+            }
+        }
+
+
+        public void Conta()
+        {
+            Console.WriteLine("Pedido: ");
+            decimal total = 0;
+
+            foreach (var item in pedidoAtual)
+            {
+                Lanches lanche = item.Key;
+                int quantidade = item.Value;
+                decimal subtotal = lanche.Preco * quantidade;
+                total += subtotal;
+
+                Console.WriteLine($"{lanche.Nome} - Quantidade: {quantidade} - Subtotal: R${subtotal:F2}");
+            }
+            decimal taxaServico = total * 0.07m;
+            decimal totalComTaxa = total + taxaServico;
+
+            Console.WriteLine($"Total: R${total:F2}");
+            Console.WriteLine($"Taxa de Serviço (7%): R${taxaServico:F2}");
+            Console.WriteLine($"Total com Serviço: R${totalComTaxa:F2}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using ProvaLanchonete.Entities;
+
+namespace ProvaLanchonete
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lanchonete lanchonete = new Lanchonete();
+
+
+            bool inicioPedido = true;
+
+            while (inicioPedido)
+            {
+                try
+                {
+                    Console.WriteLine("Escolha uma Opção: ");
+                    Console.WriteLine("1- Cardapio");
+                    Console.WriteLine("2- Fazer Pedido");
+                    Console.WriteLine("3- Conta");
+                    Console.WriteLine("4- Cadastrar novo lanche");
+                    Console.WriteLine("5- Sair");
+                    Console.WriteLine("--------------------");
+
+
+                    int opcao = int.Parse(Console.ReadLine());
+
+                    switch (opcao)
+                    {
+                        case 1:
+
+                            lanchonete.Cardapio();
+                            break;
+
+                        case 2:
+                            Console.Write("Digite o número do lanche para adicinar ao pedido: ");
+                            if (int.TryParse(Console.ReadLine(), out int indicelanche))
+                            {
+                                lanchonete.Pedido(indicelanche);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Opçao Invalida");
+                            }
+                            break;
+
+                        case 3:
+                            lanchonete.Conta();
+                            break;
+
+                        case 4:
+                            Console.Write("Nome Lanche: ");
+                            string nome = Console.ReadLine();
+
+                            Console.Write("Descrição: ");
+                            string descricao = Console.ReadLine();
+
+                            Console.Write("Preço: ");
+                            if (decimal.TryParse(Console.ReadLine(), out decimal preco))
+                            {
+                                lanchonete.AdicionarLanche(nome, descricao, preco);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Preço Inválido");
+                                Console.WriteLine("--------------------"); ;
+                            }
+                            break;
+
+                        case 5:
+                            inicioPedido = false;
+                            break;
+
+                        default:
+                            Console.WriteLine("Opção Invalida");
+                            break;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Erro, por favor digite um número");
+                    Console.WriteLine("--------------------");
+                }
+            }
+            Console.WriteLine("Obrigado pela preferencia volte sempre");
+        }
+    }
+}
